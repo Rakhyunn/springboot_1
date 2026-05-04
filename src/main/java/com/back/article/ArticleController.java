f2c989d5fede39ba8b6fffdaf619ab5372ffeb58
Rakhyunn <krh0323@pukyong.ac.kr>
2026-05-04 16:57:32 +0900

    [feat] 요구사항1 구현

---
+++ src/main/java/com/back/article/ArticleController.java
@@ -0,0 +1,12 @@
+package com.back.article;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArticleController {
+    @GetMapping("/article/list")
+    public String list() {
+        return "article_list";
+    }
+}

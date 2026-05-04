dd122e5ca6ccf8d7accd361334c5159428245e30
Rakhyunn <krh0323@pukyong.ac.kr>
2026-05-04 20:17:06 +0900

    [feat] 요구사항3, 4 구현

--- src/main/java/com/back/article/ArticleController.java
+++ src/main/java/com/back/article/ArticleController.java
@@ -1,17 +1,39 @@
 package com.back.article;
 
+import jakarta.websocket.server.PathParam;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ArticleController {
+    private final ArticleService articleService;
+
     @GetMapping("/article/list")
-    public String list() {
+    public String list(Model model) {
+        List<Article> articleList = articleService.findAll();
+        model.addAttribute(articleList);
         return "article_list";
     }
 
     @GetMapping("/article/create")
     public String create() {
         return "article_create";
+    }
+
+    @PostMapping("/article/create")
+    public String createArticle(@PathParam("title") String title, @PathParam("content") String content) {
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        articleService.save(article);
+        return "redirect:/article/list";
     }
 }
---
+++ src/main/java/com/back/article/ArticleService.java
@@ -0,0 +1,20 @@
+package com.back.article;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public Article save(Article article) {
+        return articleRepository.save(article);
+    }
+
+    public List<Article> findAll() {
+        return articleRepository.findAll();
+    }
+}
